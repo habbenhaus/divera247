8643338a36e7010626594628f583bcde5fe852ab
Hendrik Abbenhaus <kiyon1995@gmail.com>
2022-01-26 00:42:31 +0100

    add getJwt

--- src/endpoints/auth/auth.ts
+++ src/endpoints/auth/auth.ts
@@ -1,5 +1,6 @@
 import {LoginApiResult} from "./models/login-api-result.model";
 import {BaseClient} from "../../base-client";
+import {DiveraResponse} from "../divera-response.model";
 
 export class Auth extends BaseClient {
   getAccessToken(username: string, password: string): Promise<string> {
@@ -8,5 +9,9 @@ export class Auth extends BaseClient {
       password,
       jwt: false
     }).then(resp => resp.data.user?.access_token);
+  }
+
+  getJWT(): Promise<string> {
+    return this.get<DiveraResponse<{jwt: string}>>('v2/auth/jwt').then(resp => resp.data.jwt);
   }
 }
